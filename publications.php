9ca1f82abe5452d03c1af7bf9e887e18f1dd76c6
denis parra <dalpasa@yahoo.com>
2014-08-07 01:08:13 -0400

    add publications
    publications added via bibbase

---
+++ publications.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>denisparra.github.io by denisparra</title>
+
+    <link rel="stylesheet" href="stylesheets/styles.css">
+    <link rel="stylesheet" href="stylesheets/pygment_trac.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
+        <img src="http://www.ing.puc.cl/cuerpo-docente/wp-content/uploads/2013/08/parra-denis.jpg" />
+        <br/>
+        <h1>Denis Parra</h1>
+
+        <p class="view"><a href="http://web.ing.puc.cl/~dparra">Back to Home</a></p>
+        <p class="view"><a href="http://scholar.google.com/citations?user=aQ9TkcIAAAAJ">Google Scholar Page</a></p>
+      </header>
+      <section>
+        <h1>
+<a name="news" class="anchor" href="#news"><span class="octicon octicon-link"></span></a>Publications</h1>
+
+<?php $contents = file_get_contents("http://bibbase.org/show?bib=https%3A%2F%2Fwww.dropbox.com%2Fs%2Flh3hyblku4x2lu9%2Fdparra.bib%3Fdl%3D1"); print_r($contents); ?>
+
+      </section>
+      <footer>
+        <p><small>Hosted on GitHub Pages &mdash; Theme by <a href="https://github.com/orderedlist">orderedlist</a></small></p>
+      </footer>
+    </div>
+    <script src="javascripts/scale.fix.js"></script>
+    
+  </body>
+</html>
